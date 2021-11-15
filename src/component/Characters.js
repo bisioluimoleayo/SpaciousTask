fc6814c21e3327808baad4b41418e4c874915744
bisioluimoleayo <bisioluimoleayo1@gmail.com>
2021-11-15 22:36:06 +0100

    Click update

--- src/component/Characters.js
+++ src/component/Characters.js
@@ -1,16 +1,18 @@
-// import { useState } from "react";
+import { useState } from "react";
 import { characters } from "../data";
+import Sidebar from "./Sidebar";
 
 
 
 const Characters = () => {
-    // const [character, setCharacter] = useState()
+    const [Character, setCharacter] = useState(false)
+    const rollit = () => {setCharacter(!Character)}
 
     return ( 
         <div className="d-flex row">
               {
                     characters.map(char =>  {
-                        return<div className="card character my-4 mx-3 px-0">
+                        return<div className="card character my-4 mx-3 px-0" onClick={rollit}>
                         <img class="card-img-top char-img" src={char.image} alt="Card  cap" />
                         <div class="card-body">
                           <h5 class="card-title">{char.name}</h5>
@@ -18,6 +20,10 @@ const Characters = () => {
                         </div>
                       </div>
                     })
+                }
+                {
+                  Character && <Sidebar />
+
                 }
            </div>
      );
---
+++ src/component/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Sidebar() {
+    return (
+        <div  className ="">
+            Hello World
+        </div>
+    )
+}
+
+export default Sidebar
